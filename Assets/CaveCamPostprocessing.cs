45606bbc77a1b169c74bc0ef81d1dfe884f4f269
jeroenpx <jeroenpx@users.noreply.github.com>
2018-01-27 01:13:44 +0100

    Adding Unity project. First version of the "bat wave magic"!

---
+++ Assets/CaveCamPostprocessing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveCamPostprocessing : MonoBehaviour {
+
+	public Material caveMaterial;
+
+	private float startTime;
+	private float endTime;
+	private float previousEndTime;
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			previousEndTime = endTime;
+			startTime = Time.time;
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			endTime = Time.time;
+		}
+
+		caveMaterial.SetFloat ("_PreviousTimeEnd", previousEndTime);
+		caveMaterial.SetFloat ("_TimeManual", Time.time);
+		caveMaterial.SetFloat ("_TimeStart", startTime);
+		caveMaterial.SetFloat ("_TimeEnd", endTime);
+	}
+
+	/*void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		
+
+		//Graphics.Blit(src, dest, caveMaterial);
+	}*/
+}
