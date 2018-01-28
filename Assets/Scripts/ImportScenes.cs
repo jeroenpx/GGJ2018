fc000a0f7f71668d7b892987029d7bedff20cbee
Cedric <cedric.kamermans@student.howest.be>
2018-01-28 11:37:52 +0100

    Begin level scene

---
+++ Assets/Scripts/ImportScenes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ImportScenes : MonoBehaviour
+{
+
+    public Transform Endpoint;
+
+	void Start ()
+    {
+		SceneManager.LoadScene("Tunnelpiece1", LoadSceneMode.Additive);
+		SceneManager.LoadScene("Tunnelpiece1", LoadSceneMode.Additive);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
